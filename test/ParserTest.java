17f0a7b1b63b32e3273734c81ff043f7eb194c6c
Markus Klinik <mkl@lambdanaut.net>
2018-02-15 16:54:17 +0100

    test that multiplication is left associative

--- test/ParserTest.java
+++ test/ParserTest.java
@@ -57,4 +57,13 @@ public class ParserTest {
 				TokenType.TOK_PLUS, new AstExprBinOp(new AstExprInteger(2),
 						TokenType.TOK_MULT, new AstExprInteger(3))), ast);
 	}
+
+	@Test
+	public void testMultiplicationLeftAssociative() {
+		Parser p = new Parser("1*2*3");
+		AstExpr ast = p.pExpr();
+		assertEquals(new AstExprBinOp(new AstExprBinOp(new AstExprInteger(1),
+				TokenType.TOK_MULT, new AstExprInteger(2)), TokenType.TOK_MULT,
+				new AstExprInteger(3)), ast);
+	}
 }
