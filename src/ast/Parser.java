bc2b681be03f9752a59b2e378eb4bf1758e37247
Markus Klinik <mkl@lambdanaut.net>
2018-02-15 16:19:27 +0100

    comments

--- src/ast/Parser.java
+++ src/ast/Parser.java
@@ -7,6 +7,10 @@ import lex.TokenType;
 public class Parser {
 	private Lexer lexer;
 	private Token currentToken;
+	
+	// =========================================================
+	// Constructors
+	// =========================================================
 
 	public Parser(Lexer l) {
 		lexer = l;
@@ -15,19 +19,30 @@ public class Parser {
 	public Parser(String input) {
 		lexer = new Lexer(input);
 	}
+	
+	// =========================================================
+	// Helper functions
+	// =========================================================
 
+	// Helper function to fetch the next token
 	private void next() {
 		currentToken = lexer.nextToken();
 	}
 
+	// Helper function to check the type of the current token
 	private boolean match(TokenType tok) {
 		return currentToken.getTokenType() == tok;
 	}
 
+	// Helper function to generate error messages
 	private Error error(String name) {
 		return new Error(name + ": unexpected token "
 				+ currentToken.getTokenType().toString());
 	}
+	
+	// ==========================================================
+	// The actual parser
+	// ==========================================================
 
 	public AstExpr pExpr() {
 		AstExpr ast = pTerm();
