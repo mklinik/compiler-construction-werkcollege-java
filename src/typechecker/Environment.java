29a8831e99e658dbe6033260f3a3d91e16672069
Markus Klinik <mkl@lambdanaut.net>
2018-03-22 11:15:47 +0100

    fix warnings

--- src/typechecker/Environment.java
+++ src/typechecker/Environment.java
@@ -5,9 +5,12 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class Environment extends HashMap<String, Type> {
+	// just shut up, okay?
+	private static final long serialVersionUID = 42L;
+	
 	// Applies a substitution to all types in the environment
 	public void applySubstitution(Substitution s) {
-		Iterator it = this.entrySet().iterator();
+		Iterator<Map.Entry<String, Type>> it = this.entrySet().iterator();
 		while (it.hasNext()) {
 			Map.Entry<String, Type> pair = (Map.Entry<String, Type>) it.next();
 			this.put(pair.getKey(), pair.getValue().applySubstitution(s));
--- src/typechecker/Substitution.java
+++ src/typechecker/Substitution.java
@@ -4,4 +4,6 @@ import java.util.HashMap;
 
 // We extend HashMap to give it a better name
 public class Substitution extends HashMap<String, Type> {
+	// just shut up, okay?
+	private static final long serialVersionUID = 42L;
 }
